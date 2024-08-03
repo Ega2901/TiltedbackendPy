1498b71590d96775425918f134c060ae78d7af4f
Ega2901 <egortuzarov@Egors-MacBook-Air.local>
2024-08-03 15:28:34 +0500

    fefe

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root@localhost/tilted"
+SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:your_password@localhost/tilted"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
